fd3ecff21d74a642273802858a829d19f239c50b
Ethan Hammond <ethan.hammond000@gmail.com>
2014-10-09 14:55:17 -0500

    commit

--- RombergIntegration.cpp
+++ RombergIntegration.cpp
@@ -11,27 +11,22 @@ double RombergIntegration::accurateRomberg(MultiVarFunction* f, double a, double
 {
    Double* db;  //use this variable to place and retrieve values on the queue
    
-   QueueLinked<Double>* q1 = new QueueLinked<Double>;
-   QueueLinked<Double>* q2 = new QueueLinked<Double>;
+   QueueLinked<Double>* q1 = new QueueLinked<Double>();
+   QueueLinked<Double>* q2 = new QueueLinked<Double>();
 
 
    int counter = 0;
    int n = 1;  //current number of intervals
-   while (                     )
+   while (counter <= level)
    {
       //DO THIS
       //obtain the required number of trapezoid evaluations depending on the number of levels requested
       //put all of the level 0 results on the q1
 		
 	  
+      db = romberg(f, a, b, n);
 
-
-
-
-
-
-
-
+      q1->enqueue(db);
 
       n = 2*n;  //double the number of intervals
       counter++;
