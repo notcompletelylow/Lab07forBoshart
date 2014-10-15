952917aa6388a003016fa8d860d7775eb121af37
Ethan Hammond <ethan.hammond000@gmail.com>
2014-10-15 13:53:53 -0500

    finished RombergIntegration.cpp

--- RombergIntegration.cpp
+++ RombergIntegration.cpp
@@ -34,36 +34,37 @@ double RombergIntegration::accurateRomberg(MultiVarFunction* f, double a, double
 
    //q1 now has all of the level 0 integration results
 
-   double factor;  //use this to compute the current Romberg Factor (4^k stuff)
+   double factor = pow(4, 1);  //use this to compute the current Romberg Factor (4^k stuff)
    int power = 1;  //k, used to compute the Romberg Factor
 
    //if level 0 has been requested, the loop will not execute
    //the total number of executions of the loop is ??
 
    //DO THIS
-   int iterations = pow(2, level);            //can be precomputed
+   int iterations = level * (level + 1) / 2;            //can be precomputed
    while (iterations > 0)
    {
       //DO THIS
       //use the algorithm described in the lab to improve the accuracy of your level 0 results
 
 
+      double less = q1->dequeue()->getValue();
+      double more = q1->peek()->getValue();
 
+      q2->enqueue(((factor * more) - less) / (factor - 1));
+     
+      if (q1->size() == 1)
+      {
+         q1->dequeue();
 
+         for (i = q2->size(); i > 0; i--;)
+         {
+            q1->enqueue(q2->dequeue());
+         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+         power++;
+         factor = pow(4, power);
+      }
 
       iterations--;
    }
